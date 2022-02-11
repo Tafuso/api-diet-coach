2b05f227e526d2a731468aac59e5046d8ca7dcd8
Tafuso <gustavovasquez2002@gmail.com>
2022-02-11 14:32:56 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as express from "express";
 import routes from "./database/Routes"
+import * as cors from "cors"
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
